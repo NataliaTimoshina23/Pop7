88a3ce6f6f5147509de61b3009fad1617a9a498a
Natalia Timoshina <FaustoffNatalia@yandex.ru>
2024-11-13 16:50:23 +0300

    git reset --hard HEAD~1

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+    public int calculate (double weight, double height) {
+        double math = height * height;
+        double index = weight / math;
+        return (int) index;
+    }
+}
+
